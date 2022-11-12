b389f1c4cd41315675740669a4d23225eea07fa1
dev <dev@dev.com>
2022-11-12 17:24:12 +0800

    update docker-compose

---
+++ build_w3pra_image.sh
@@ -0,0 +1,16 @@
+#mvn clean install
+
+cd ./rpa-server/rpa-server-start
+
+group=web3b
+artifactId=w3rpa-server
+version=1.0.0
+
+docker build -t ${artifactId}:${version}  .
+#docker build -t ${artifactId}:${version} -f DockfileBoot .
+
+##
+#docker login hkccr.ccs.tencentyun.com --username=xxxx
+docker tag ${artifactId}:${version} hkccr.ccs.tencentyun.com/web3b/${artifactId}:${version}
+docker push hkccr.ccs.tencentyun.com/web3b/${artifactId}:${version}
+
